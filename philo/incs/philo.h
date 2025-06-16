f4ec5e16975278a4173df2cc5330fe03ae002f06
Carl Castro <ccastro@lab2r3s11.42abudhabi.ae>
2025-06-16 17:19:46 +0400

    skeleton

---
+++ philo/incs/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 15:22:55 by ccastro           #+#    #+#             */
+/*   Updated: 2025/06/16 17:03:27 by ccastro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
