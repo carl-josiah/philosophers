3aaa8c6475245228593737dd57c0d9df351a001b
Carl Castro <ccastro@lab2r3s11.42abudhabi.ae>
2025-06-16 17:21:54 +0400

    removed

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,29 +6,13 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:22:37 by ccastro           #+#    #+#             */
-/*   Updated: 2025/06/16 17:12:21 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/06/16 17:21:45 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/philo.h"
 
-void	*hello(void *arg)
+int	main(int ac, char *av[])
 {
-	(void) arg;
-    printf("Hello from thread\n");
-	sleep(120);
-	printf("Dead\n");
-	return (NULL);
-}
-
-int main(void)
-{
-    pthread_t	thread1;
-	pthread_t	thread2;
-
-	pthread_create(&thread1, NULL, &hello, NULL);
-	pthread_create(&thread2, NULL, &hello, NULL);
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
 	return (0);
 }
