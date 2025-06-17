a9a5bb0a57ce600ac2501170c3a8271c7229065f
Carl Castro <ccastro@lab2r2s11.42abudhabi.ae>
2025-06-17 18:08:34 +0400

    change av[i] to ac

--- philo/srcs/parsing.c
+++ philo/srcs/parsing.c
@@ -6,18 +6,11 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 15:16:55 by ccastro           #+#    #+#             */
-/*   Updated: 2025/06/17 17:38:07 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/06/17 18:02:49 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/philo.h"
-
-int	is_valid_argc(int ac)
-{
-	if (ac == 5 || ac == 6)
-		return (1);
-	return (0);
-}
 
 static int	is_empty_arg(int ac, char *av[])
 {
@@ -71,26 +64,35 @@ static int	is_all_digit(int ac, char *av[])
 	return (1);
 }
 
-static int	is_valid_num(char *av[])
+static int	is_valid_num(int ac, char *av[])
 {
 	int		i;
 	int		num;
 
 	i = 1;
-	while (av[i])
+	while (i < ac)
 	{
 		if (!philo_atoi_safe(av[i], &num))
+			return (0);
+		if (num <= 0)
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
+int	is_valid_argc(int ac)
+{
+	if (ac == 5 || ac == 6)
+		return (1);
+	return (0);
+}
+
 int	is_valid_argv(int ac, char *av[])
 {
 	if (is_empty_arg(ac, av) || !is_all_digit(ac, av))
 		return (0);
-	if (!is_valid_num(av))
+	if (!is_valid_num(ac, av))
 		return (0);
 	return (1);
 }
