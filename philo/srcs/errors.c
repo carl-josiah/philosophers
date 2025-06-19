835944ccc6e610a295ea73d0b5d84ff4691d7919
carl-josiah <carl_josiah@yahoo.com>
2025-06-19 18:20:27 +0000

    errors

--- philo/srcs/errors.c
+++ philo/srcs/errors.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 14:54:29 by ccastro           #+#    #+#             */
-/*   Updated: 2025/06/19 14:09:32 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/06/19 18:20:18 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ void	error_message(const char *msg)
 	printf(RED "ERROR: %s\n" DEFAULT, msg);
 	printf("Usage: ./philo ");
 	printf("[philosopher_count] [time_to_die] [time_to_eat] [time_to_sleep] ");
-	printf(GREY "optional: [req_meal_count]\n");
+	printf(GREY "optional: [req_meal_count]\n" DEFAULT);
 }
