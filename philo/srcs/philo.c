e7144f305dc07b6a39a757cdb96a4d4c2317ddee
Carl Castro <ccastro@lab1r3s7.42abudhabi.ae>
2025-06-25 18:33:22 +0400

    philo

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:22:37 by ccastro           #+#    #+#             */
-/*   Updated: 2025/06/25 09:30:51 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/06/25 17:40:22 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	main(int ac, char **av)
 {
+	t_philo	philo;
+	t_info	info;
+	
 	if (!is_correct_input(ac, av))
+		return (EXIT_FAILURE);
+	if (!init(&philo, &info))
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
